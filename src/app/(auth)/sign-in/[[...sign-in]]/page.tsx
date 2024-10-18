00d1664d46a6f37db062cda59338aa826ad14578
SupremeMasterK <142467843+SupremeMasterK@users.noreply.github.com>
2024-10-18 14:20:23 +0530

    clerk auth sign and sign out completed

---
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,23 @@
+import { SignIn, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="h-full lg:flex flex-col items-center justify-center px-4">
+        <div className="flex items-center justify-center mt-8">
+          <ClerkLoaded>
+            <SignIn path="/sign-in" />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground" />
+          </ClerkLoading>
+        </div>
+      </div>
+      <div className="h-full bg-green-300 hidden lg:flex items-center justify-center">
+        <Image src="/images/acc_logo.png" alt="logo" width={200} height={200} />
+      </div>
+    </div>
+  );
+}
--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center gap-4 mt-10">
+      <UserButton afterSwitchSessionUrl="/" />
       <h1 className="text-3xl font-bold ">Welcome!</h1>
-      <Button variant={"outline"}>Dashboard Button</Button>
+      <p>This is an Authenticated Route</p>
     </div>
   );
 }
